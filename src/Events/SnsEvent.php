096e116d8c6ad892b800b8932925837fcf520ab6
Alex Renoki <rennokki@gmail.com>
2020-01-16 22:02:51 +0200

    Fixed configuration.

--- src/Events/SnsEvent.php
+++ src/Events/SnsEvent.php
@@ -10,11 +10,17 @@ class SnsEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    /**
+     * The message to be delivered in the listeners.
+     *
+     * @var array
+     */
     public $message;
 
     /**
      * Create a new event instance.
      *
+     * @param  array  $message
      * @return void
      */
     public function __construct($message)
