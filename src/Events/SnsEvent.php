dee995a6b1ca71a2bdb7d20a9de983f9c745fcd7
Alex Renoki <rennokki@gmail.com>
2020-06-24 20:41:15 +0300

    Removed traits from the event

--- src/Events/SnsEvent.php
+++ src/Events/SnsEvent.php
@@ -2,14 +2,8 @@
 
 namespace Rennokki\LaravelSnsEvents\Events;
 
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-
 class SnsEvent
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
     /**
      * The payload to be delivered in the listeners.
      *
